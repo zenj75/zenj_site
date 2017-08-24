9ef09da4ac185af7f3c6408cb23e6f43ac2e751c
Evgeny Zakharov <zenj75@gmail.com>
2017-08-24 16:30:18 +0300

    проект_v2

--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,14 @@ def main_view(request):
     return render_to_response('index.html')
 
 def school_view(request):
-    return render_to_response('school.html')
+    learns = ['Димитровградский Механико-технологический Колледж Молочной Промышленности',
+              'Институт международного права и экономики имени А.С. Грибоедова', 'различные курсы Cisco и Solaris']
+    return render_to_response('school.html', {'learns': learns} )
 
 def work_view(request):
-    return render_to_response('work.html')
+    my_works = ['Департамент по государственной регистрации прав на недвижимое имущество г.Димитровграда',
+             'Димитровградский автоагрегатный завод', 'МЕГАФОН (Санкт-Петербург)', 'Северен-Телеком (Санкт-Петербург)',
+             'МГТС (Москва)']
+    return render_to_response('work.html', {'my_works':my_works} )
+
 
